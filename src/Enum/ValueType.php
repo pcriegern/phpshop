c6682a75d4b7d49aee41a12f8118927aee0acca0
DeepCommerce <dev@deepcommerce.cloud>
2024-06-04 14:28:00 +0200

    Add files via upload

---
+++ src/Enum/ValueType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Deepcommerce\Phpshop\Enum;
+
+/**
+ * Class ValueType
+ */
+class ValueType {
+    const STRING   = 'string';
+    const TEXT     = 'text';
+    const INTEGER  = 'integer';
+    const NUMBER   = 'number';
+    const BOOLEAN  = 'boolean';
+    const DATE     = 'date';
+    const TIME     = 'time';
+    const DATETIME = 'datetime';
+    const JSON     = 'json';
+    const FILE     = 'file';
+    const IMAGE    = 'image';
+    const VIDEO    = 'video';
+    const AUDIO    = 'audio';
+    const URL      = 'url';
+    const EMAIL    = 'email';
+}
+
---
+++ src/Enum/Visibility.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Deepcommerce\Phpshop\Enum;
+
+/**
+ * Class Visibility 
+ */
+class Visibility {
+    const PLP       = 'plp';
+    const PDP       = 'pdp';
+    const INVISIBLE = 'invisible';
+}
