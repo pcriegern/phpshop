8a03c0be01751de0c0728ce62a9df43afa5a01d9
DeepCommerce <dev@deepcommerce.cloud>
2024-06-04 14:26:39 +0200

    Create AttributeType.php

---
+++ src/Enum/AttributeType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Deepcommerce\Phpshop\Enum;
+
+/**
+ * Class AttributeType
+ */
+class AttributeType {
+    const DESCRIPTIVE           = 'descriptive';
+    const DISTINCTIVE           = 'distinctive';
+    const QUANTITATIVE_VARIABLE = 'quantitative';
+    const QUALITATIVE_VARIABLE  = 'qualitative';
+}
